e1c937ff9e2ac3324132c9dfa6ee362c0ad77266
邝嘉俊 <kuangjiajun@yushu.biz>
2025-03-26 18:30:17 +0800

    feat: 增加图片

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="home">
+        <div class="name">
+            <img src="../assets/name.png" alt="" />
+        </div>
+        <div class="img" />
+    </div>
+</template>
+
+<style scoped lang="less">
+.home {
+    width: 100%;
+    height: 100%;
+    background: #000;
+    color: #fff;
+    position: relative;
+
+    .img {
+        background: url('../assets/img_1.png') no-repeat center center;
+        background-size: cover;
+        width: 100%;
+        height: 100%;
+    }
+
+    .name {
+        position: absolute;
+        left: 50%;
+        top: 50px;
+        transform: translateX(-50%) scale(0.5);
+    }
+}
+</style>
