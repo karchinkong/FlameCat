a72e3920c95c0f4430bed9d6cd48d54d9def599e
邝嘉俊 <kuangjiajun@yushu.biz>
2025-03-26 18:36:39 +0800

    feat: 更换路由方式

--- components.d.ts
+++ components.d.ts
@@ -9,14 +9,5 @@ declare module 'vue' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    VanActionBar: typeof import('vant/es')['ActionBar']
-    VanActionBarButton: typeof import('vant/es')['ActionBarButton']
-    VanActionBarIcon: typeof import('vant/es')['ActionBarIcon']
-    VanCell: typeof import('vant/es')['Cell']
-    VanCellGroup: typeof import('vant/es')['CellGroup']
-    VanCol: typeof import('vant/es')['Col']
-    VanSwipe: typeof import('vant/es')['Swipe']
-    VanSwipeItem: typeof import('vant/es')['SwipeItem']
-    VanTag: typeof import('vant/es')['Tag']
   }
 }
