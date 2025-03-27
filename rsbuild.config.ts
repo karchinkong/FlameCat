645d5a3def1c4fbfeecc79319ae3e6de19f1e1f3
邝嘉俊 <kuangjiajun@yushu.biz>
2025-03-27 18:35:54 +0800

    feat: 增加图片压缩功能

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,10 +4,9 @@ import { pluginLess } from "@rsbuild/plugin-less";
 import AutoImport from "unplugin-auto-import/rspack";
 import Components from "unplugin-vue-components/rspack";
 import { VantResolver } from "@vant/auto-import-resolver";
-import { pluginImageCompress } from '@rsbuild/plugin-image-compress';
 
 export default defineConfig({
-  plugins: [pluginVue(), pluginLess(), pluginImageCompress()],
+  plugins: [pluginVue(), pluginLess()],
   tools: {
     rspack: {
       plugins: [
